cfb007625d76fa69b4f0f4683cbe8a4cc48b9027
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-20 23:38:46 +0530

    Update stringmethods.py
    typecasting

--- stringmethods.py
+++ stringmethods.py
@@ -1,37 +1,37 @@
-#string methods (str)
+string methods (str)
 
-#name = "Hello dude"
+name = "Hello dude"
 
-#print(len(name))
-#print(name.find("o"))
-#print(name.isdigit())
-#print(name.isalpha())
-#print(name.capitalize())
-#print(name.upper())
-#print(name.lower())
-#print(name.replace("e","a"))
-#print(name.count("d"))
-#print(name*8)
+print(len(name))
+print(name.find("o"))
+print(name.isdigit())
+print(name.isalpha())
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.replace("e","a"))
+print(name.count("d"))
+print(name*8)
 
 #TYPE CASTING = convert data type of a value to another data type
 
-#x=5     #int
-#y=3.0   #float
-#z="7"   #str
+x=5     #int
+y=3.0   #float
+z="7"   #str
 
-#y=int(y)
-#z=int(z)
+y=int(y)
+z=int(z)
 
-#x=float(x)
-#y=float(y)
-#z=float(z)
+x=float(x)
+y=float(y)
+z=float(z)
 
-#print(x)
-#print(y)
-#print(int(y))
-#print(z*4)
+print(x)
+print(y)
+print(int(y))
+print(z*4)
 
 #when whould you need to convert a integer or float to a string?
-#print("X is "+ str(x))
-#print("Y is "+str(y))
-#you need to type cast when you need to reprent a string along with integer or float
+print("X is "+ str(x))
+print("Y is "+str(y))
+you need to type cast when you need to reprent a string along with integer or float
